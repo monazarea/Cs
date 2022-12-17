ca5fdb91b4d7abadd394e07c9aa9abd0976fc276
monazarea <94869444+monazarea@users.noreply.github.com>
2022-12-17 21:02:00 +0200

    Time: 0 ms (100.00%), Space: 8.5 MB (10.07%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,36 +1,17 @@
-// class Solution {
-// public:
-//     int sumOddLengthSubarrays(vector<int>& arr) {
-//         vector<int>v(arr.size()+1);
-//         v[0]=0;
-//         for(int i=0;i<arr.size();i++){
-//             v[i+1]=arr[i]+v[i];
-//         }
-//         int sum=0;
-//     for(int i=1;i<v.size();i++){
-//         for(int j=i;j<v.size();j+=2){
-//             sum+=(v[j]-v[i-1]);
-//         }
-//     }
-//     return sum;
-//     }
-// };
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int n = int(arr.size()), answer = 0;
-        
-        for (int left = 0; left < n; ++left) {
-            for (int right = left; right < n; ++right) {
-                if ((right - left + 1) % 2 == 1) {
-                    int currentSum = 0;
-                    for (int index = left; index < right + 1; ++index) {
-                        currentSum += arr[index];    
-                    }
-                    answer += currentSum;
-                }
-            }
+        vector<int>v(arr.size()+1);
+        v[0]=0;
+        for(int i=0;i<arr.size();i++){
+            v[i+1]=arr[i]+v[i];
         }
-        return answer;
+        int sum=0;
+    for(int i=1;i<v.size();i++){
+        for(int j=i;j<v.size();j+=2){
+            sum+=(v[j]-v[i-1]);
+        }
+    }
+    return sum;
     }
 };
