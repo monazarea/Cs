d1a4e5bc293edd2ed9e86a311030346252d23758
monazarea <94869444+monazarea@users.noreply.github.com>
2023-02-17 18:57:28 +0200

    Time: 16 ms (60.81%), Space: 6 MB (15.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }int revert = 0;
+        while(x > revert) {
+            revert = revert * 10 + x % 10;
+            x /= 10;
+        } return x == revert|| x == revert/10;
+    }
+};
